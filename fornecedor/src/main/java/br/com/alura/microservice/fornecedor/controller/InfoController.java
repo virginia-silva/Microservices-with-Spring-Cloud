8df0b5254c792320cc027a4023db949702e57519
Virginia Silva <virginia_hol@hotmail.com>
2022-02-07 08:20:35 -0300

    Load Balancer e Spring Feign

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -14,7 +14,7 @@ public class InfoController {
 	
 	@Autowired
 	private InfoService infoService;
-
+	
 	@RequestMapping("/{estado}")
 	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
 		return infoService.getInfoPorEstado(estado);
